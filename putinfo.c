325aaa9d96d69df24cec3103419fc18a770f14c1
Aurélien Cedeyn <aurelien.cedeyn@gmail.com>
2021-08-07 01:04:29 +0200

    putinfo: Update sqlite behaviour and add significant improvement
    * Add timestamp management
    * Correctly manage SQL_BUSY return code
    * Check if db exists before opening it
    * Manage options in main
    * Added todo
    * Added former function prototypes

--- putinfo.c
+++ putinfo.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <sqlite3.h>
 #include <errno.h>
 #include <stdio.h>
@@ -5,10 +7,46 @@
 #include <string.h>
 #include <stdbool.h>
 #include <time.h>
+#include <unistd.h>
+
+#include <getopt.h>
 
 #define DBFILE "/tmp/base.sqlite"
 
+#define MAX_ATTEMPTS 5
+
 char **results = NULL;
+
+typedef struct _info {
+	/* Key from info */
+	char *key;
+	/* Value from info */
+	char *value;
+	/* Timestamp of the record */
+	long ts;
+	/* Info type */
+	const char* type;
+	/* Id */
+	long id;
+} info;
+
+/*
+ * TODO:
+ * - main: Gestion de l'initialisation de la base de donnée sqlite
+ *   (voir si la table existe avant de l'initialiser).
+ *
+ * - putinfo: Meilleurs résolution du timestamp en utilisation clock_gettime.
+ *
+ * - getinfo: Récupération de toutes les valeurs dans results.
+ *
+ * - main: Gestion du nettoyage de la base de donnée (suppression des entrées
+ *   au delà d'un timestamp).
+ *
+ * - getinfo: Meilleure gestion de la récupération des résultats (Libération
+ *   de la mémoire allouée pour results).
+ *
+ * - main: Vérifier la compatibilité avec l'API existante.
+ * */
 
 int get_max_length(sqlite3 *db){
 	int max_request_size;
@@ -24,29 +62,38 @@ int putinfo(sqlite3 *db, int id, char *key, char *value, char *type) {
 	int rc;
 	int max_request_size;
 	char *sql;
+	struct timespec tp;
 	max_request_size = get_max_length(db);
 
 	sql = (char *) malloc(max_request_size);
 	if (sql == NULL) {
-		perror("Unable to alloc buffer");
+		perror("putinfo: Unable to alloc buffer");
 		return 1;
 	}
-	snprintf(sql, max_request_size, "INSERT INTO Vars VALUES(%d, '%s', '%s', '%s', %ld);", id, key, value, type, time(NULL));
+	
+	if (clock_gettime(CLOCK_MONOTONIC, &tp)) {
+		perror("putinfo: Unable to get time");
+		return 1;
+	}
+	//snprintf(sql, max_request_size, "INSERT INTO Vars VALUES(%d, '%s', '%s', '%s', %ld);", id, key, value, type, time(NULL));
+	snprintf(sql, max_request_size, "INSERT INTO Vars VALUES(%d, '%s', '%s', '%s', %ld);", id, key, value, type, tp.tv_sec);
+	//snprintf(sql, max_request_size, "INSERT INTO Vars VALUES(%d, '%s', '%s', '%s', %ld.%ld);", id, key, value, type, tp.tv_sec, tp.tv_nsec);
 
-	rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
-    
+	while ((rc = sqlite3_exec(db, sql, 0, 0, &err_msg)) == SQLITE_BUSY){
+		continue;
+	}
+
 	if (rc != SQLITE_OK ) {
-		fprintf(stderr, "SQL error: %s\n", err_msg);
-		sqlite3_free(err_msg);        
+		fprintf(stderr, "putinfo: SQL error: %s\n", err_msg);
+		sqlite3_free(err_msg);
 		sqlite3_close(db);
 		rc = 1;
-	} 
+	}
 	free(sql);
 	return rc;
 }
 
-int get_result(char ***result, int argc, char **argv, 
-                    char **azColName) {
+int get_result(char ***result, int argc, char **argv, char **azColName) {
 	/* 2 fields per entry: Key value (argv[0]) and timestamp (argv[1])*/
 	bool first = false;
 
@@ -88,57 +135,215 @@ int getinfo(sqlite3 *db, int id, char *key, char *type) {
 
 	sql = (char *) malloc(max_request_size);
 	if (sql == NULL) {
-		perror("Unable to alloc buffer");
+		perror("getinfo: Unable to alloc buffer");
 		return 1;
 	}
-	snprintf(sql, max_request_size, "SELECT Value, Ts FROM Vars WHERE Type='%s' AND Key='%s' AND ID='%d';", type, key, id);
+	snprintf(sql, max_request_size, "SELECT Value, Ts FROM Vars WHERE Type='%s' AND Key='%s' AND ID='%d' ORDER BY Ts LIMIT 1;", type, key, id);
 
-        rc = sqlite3_exec(db, sql, (void *) get_result, &results, &err_msg);
+	while ((rc = sqlite3_exec(db, sql, (void *) get_result, &results, &err_msg)) == SQLITE_BUSY){
+		continue;
+	}
 	if (rc != SQLITE_OK ) {
-		fprintf(stderr, "SQL error: %s\n", err_msg);
-		sqlite3_free(err_msg);        
+		fprintf(stderr, "getinfo: SQL error: %s\n", err_msg);
+		sqlite3_free(err_msg);
 		sqlite3_close(db);
 		rc = 1;
-	} 
+	} else {
+		printf("getinfo: results: %p\n", results);
+		print_results(results);
+	}
 	free(sql);
-
-	printf("results: %p\n", results);
-	print_results(results);
 	return rc;
 }
 
-int main(int argn, char **argv, char **arge) {
+int check_db(char *dbfile) {
+	int rc;
+	sqlite3 *db;
+
+	rc = sqlite3_open_v2(dbfile, &db, SQLITE_OPEN_FULLMUTEX |
+					  SQLITE_OPEN_READONLY, NULL);
+	if (rc == SQLITE_OK) {
+		sqlite3_close(db);
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+sqlite3 * open_db(char *dbfile){
+	int rc;
+	sqlite3 *db;
+
+	rc = sqlite3_open_v2(dbfile, &db, SQLITE_OPEN_FULLMUTEX |
+					  SQLITE_OPEN_READWRITE |
+					  SQLITE_OPEN_CREATE, NULL);
+	if (rc == SQLITE_OK){
+		if (sqlite3_busy_timeout(db, (rand() + 100) % 1000) != SQLITE_OK)
+			perror("open_db: Unable to set busy timeout");
+		return db;
+	}
+	return NULL;
+}
+
+/*
+ * cleanup:
+ *
+ * Cleanup database history from the given timestamp
+ *
+ * */
+int cleanup(sqlite3 *db, struct timespec timestamp){
+	return 0;
+}
+
+sqlite3 * init_db(char *dbfile){
+	/* Initialize Key Value Store (KVS) database */
 	sqlite3 *db;
 	char *err_msg = 0;
 	int rc;
 
-	if (sqlite3_open(DBFILE, &db) != SQLITE_OK) {
-		perror("Unable to open db");
-		exit(1);
+	db = open_db(dbfile);
+	if (db == NULL) {
+		perror("init_db: Unable to open db");
+		return NULL;
 	}
 	
-	char *sql = "DROP TABLE IF EXISTS Vars;" 
+	char *sql = "DROP TABLE IF EXISTS Vars;"
 		    "CREATE TABLE Vars(Id INT, Key TEXT, Value TEXT, Type TEXT, Ts BIGINT);";
 
 	rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
-    
+
 	if (rc != SQLITE_OK ) {
-		fprintf(stderr, "SQL error: %s\n", err_msg);
-		sqlite3_free(err_msg);        
+		fprintf(stderr, "init_db: SQL error: %s\n", err_msg);
+		sqlite3_free(err_msg);
 		sqlite3_close(db);
-		return 1;
-	} 
-
+		return NULL;
+	}
 	printf("Init successfull\n");
-	putinfo(db, 1, "Test", "41400", "INTER");
-	putinfo(db, 1, "Test", "bouuu", "INTER");
-	putinfo(db, 1, "Toto", "bouuu", "INTER");
-	putinfo(db, 1, "Test", "41400", "BATCH");
+	return db;
+}
 
-	getinfo(db, 1, "Test", "INTER");
-	printf("Getinfo successfull\n");
-    
+/*
+int  getinfo(char *nom, char* valeur)
+int  putinfo(char *nom, char* valeur)
+*/
+
+struct _options {
+    char *database;
+    bool all;
+    bool reset;
+    bool explain;
+    char *variable;
+    char *value;
+} options;
+
+void help(char **argv){
+	printf("%s [-a] [-d <directory>] [-e] [-r] variable [value]\n"
+	       "    -a --all:            Display all variables.\n"
+	       "    -d --database=<arg>: Database file.\n"
+	       "    -e --explain:        Explain what will be done.\n"
+	       "    -r --reset:          Reset all variables\n", argv[0]);
+}
+
+int main(int argc, char **argv, char **arge) {
+	char opt;
+	char *dbfile = NULL;
+	sqlite3 *db = NULL;
+	struct timespec ts;
+	/*
+	 * Options à gérer:
+	 * -a: Donne l'ensemble des clefs/valeur de la base de donnée
+	 * -d <arg>: Répertoire info à utiliser (gestion du dbinfo) (GLOBAL_TMPDIR ou /tmp + SLURM_JOB_ID) Si ENVIRONMENT=BATCH
+	 * -e: ecriture sur stderr du résultat de la commande putinfo
+	 * -r: purge complète si pas d'argument, et RAZ de la variable si un argument (reset)
+	 * */
+
+	static struct option long_options[] =
+	{
+	    {"all", no_argument, NULL, 'a'},
+	    {"dir", required_argument, NULL, 'd'},
+	    {"explain", no_argument, NULL, 'e'},
+	    {"reset", optional_argument, NULL, 'r'},
+	    {NULL, 0, NULL, 0}
+	};
+
+	while ((opt = getopt_long(argc, argv, "ad:erh", long_options, NULL)) != -1)
+	{
+	    // check to see if a single character or long option came through
+	    switch (opt)
+	    {
+		 // short option 'a'
+		 case 'a':
+		     options.all = true;
+		     break;
+		 // short option 'd'
+		 case 'd':
+		     options.database = optarg; // or copy it if you want to
+		     break;
+		 // short option 'e'
+		 case 'e':
+		     options.explain = true; // or copy it if you want to
+		     break;
+		 // short option 'r'
+		 case 'r':
+		     options.reset = true; // or copy it if you want to
+		     break;
+		 default:
+		     help(argv);
+		     exit(1);
+	    }
+	}
+
+	if (optind < argc)
+		options.variable = argv[optind++];
+	else
+		options.variable = NULL;
+	if (optind < argc)
+		options.value = argv[optind++];
+	else
+		options.value = NULL;
+	if (optind < argc){
+		fprintf(stderr, "Too many argument.\n");
+		help(argv);
+		exit(1);
+	}
+
+	printf("main: all=%d directory=%s explain=%d reset=%d variable=%s value=%s\n",
+		options.all, options.database, options.explain, options.reset,
+		options.variable, options.value);
+
+	if (options.database == NULL){
+		if ((dbfile = secure_getenv("DBINFO")) == NULL)
+			dbfile = DBFILE;
+	} else {
+		dbfile = options.database;
+	}
+
+	printf("main: opening db from: %s\n", dbfile);
+
+	if (! check_db(dbfile)) {
+		db = init_db(dbfile);
+	} else {
+		db = open_db(dbfile);
+	}
+	if (db == NULL) {
+		perror("main: Unable to open db");
+		exit(1);
+	}
+
+	if (options.variable){
+		if (options.value)
+			putinfo(db, 1, options.variable, options.value, "INTER");
+		else
+			getinfo(db, 1, options.variable, "INTER");
+	} else {
+		if (options.all)
+			getinfo(db, 1, "%", "INTER");
+		if (options.reset)
+			cleanup(db, ts);
+	}
+
 	sqlite3_close(db);
 	return 0;
 }
 
+/*vim:noexpandtab:ts=8:sw=8*/
